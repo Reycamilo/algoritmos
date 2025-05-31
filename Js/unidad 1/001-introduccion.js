3054344f5999a50393607f18f1c32383e5178c51
Reycamilo <josecamilo2404@gmail.com>
2025-05-31 15:02:04 -0600

    se agregaron carpetas de unidad

---
+++ Js/unidad 1/001-introduccion.js
@@ -0,0 +1,7 @@
+//alert("hola como estamos.")
+
+const nombre = prompt("Cual es su nombre?");
+
+document.querySelector('.contenido')
+.innerHTML = `${nombre} esta estudiando Ingeniera en Sistemas.`
+

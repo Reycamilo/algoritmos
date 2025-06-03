ab175f764d8ca1d3215fb8cf9edc010c6311d764
Reycamilo <josecamilo2404@gmail.com>
2025-06-03 13:30:03 -0600

    002-Variables Var

--- Js/unidad1/001-introduccion.js
+++ Js/unidad1/001-introduccion.js
@@ -1,7 +1,15 @@
 //alert("hola como estamos.")
 
-const nombre = prompt("Cual es su nombre?");
+const nombre = prompt("Cual es su nomlbre?")
+
+// console.log(nombre);
+
+
 
 document.querySelector('.contenido')
 .innerHTML = `${nombre} esta estudiando Ingeniera en Sistemas.`
 
+console.log("hola mundo");
+
+
+// esta es la forma de crear un comentario 
---
+++ Js/unidad1/002-variables-var.js
@@ -0,0 +1,26 @@
+function ejemplo () {
+    var x = 12
+
+    if(true) {
+        var x = 15;
+    }
+
+    console.log(x);
+    
+}
+
+// variables se pueden inicializar 
+var producto = "Semitas"
+console.log(producto);
+
+// las varibales se pueden reasignar
+producto = "Cafe"
+console.log(producto);
+
+producto = 25;
+console.log(producto);
+
+var disponible = true;
+console.log(disponible);
+
+var categoria = 'Panes', marca = "La polular", calificacion = 9;

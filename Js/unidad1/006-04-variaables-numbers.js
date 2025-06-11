d3b006614ac8a79382eae8487fafd49642fbc49b
Reycamilo <josecamilo2404@gmail.com>
2025-06-11 06:11:47 -0600

    Se agregaron los operadores

---
+++ Js/unidad1/006-04-variaables-numbers.js
@@ -0,0 +1,39 @@
+let numero = 5
+console.log(numero);
+
+// Incrementar
+numero++;
+console.log(numero);
+
+++numero
+console.log(numero);
+
+numero--
+console.log(numero);
+
+numero += 3
+console.log(numero);
+
+
+// Convertir valores
+
+const numero1 = '18'
+const numero2 = '18.02'
+const numero3 = 'Uno';
+const numero4 = 30;
+
+
+console.log(numero1, typeof(numero1));
+console.log(Number.parseInt(numero1), typeof(Number.parseInt(numero1)));
+
+console.log(Number.parseInt(numero3), typeof(Number.parseInt(numero3)));
+
+console.log(Number.isInteger(numero4));
+
+
+
+
+
+
+
+
---
+++ Js/unidad1/007-01-variaables-operators.js
@@ -0,0 +1,10 @@
+const numero1 = 20
+const numero2 = '40'
+const numero3 = 30
+
+// mayor que
+console.log(`El numero ${numero1} es mayor que ${numero3}: ${numero1 > numero3}`);
+console.log(`El numero ${numero3} es mayor que ${numero1}: ${numero3 > numero1}`);
+console.log(`El numero ${numero3} es mayor que ${numero2}: ${numero3 > numero2}`);
+
+
